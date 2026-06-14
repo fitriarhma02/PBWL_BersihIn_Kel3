7117843c63783be8eaa91985eee345dce3f437a1
defina-ra <definarahmayanti@gmail.com>
2026-06-14 10:17:40 +0700

    fix: correct helpers.php content

--- laravel/app/helpers.php
+++ laravel/app/helpers.php
@@ -1,15 +1,16 @@
-Set-Content -Path "app/helpers.php" -Value '<?php
-if (!function_exists('"'"'kirimNotif'"'"')) {
-    function kirimNotif($userId, $title, $message, $type = '"'"'info'"'"', $icon = '"'"'info'"'"') {
-        \DB::table('"'"'notifications'"'"')->insert([
-            '"'"'user_id'"'"'    => $userId,
-            '"'"'title'"'"'      => $title,
-            '"'"'message'"'"'    => $message,
-            '"'"'type'"'"'       => $type,
-            '"'"'icon'"'"'       => $icon,
-            '"'"'is_read'"'"'    => false,
-            '"'"'created_at'"'"' => now(),
-            '"'"'updated_at'"'"' => now(),
+<?php
+
+if (!function_exists('kirimNotif')) {
+    function kirimNotif($userId, $title, $message, $type = 'info', $icon = 'info') {
+        \DB::table('notifications')->insert([
+            'user_id'    => $userId,
+            'title'      => $title,
+            'message'    => $message,
+            'type'       => $type,
+            'icon'       => $icon,
+            'is_read'    => false,
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
     }
-}'
+}
